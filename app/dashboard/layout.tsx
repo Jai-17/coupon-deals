9497fbe615c6dac7c4bf073406190619b1b0ebf8
Jai-17 <madhukarjai9@gmail.com>
2024-11-06 00:56:57 +0530

    drizzle setup

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,8 +1,9 @@
 import { ReactNode } from "react";
+import { NavBar } from "./_components/NavBar";
 
 export default function DashboardLayout({children} : {children: ReactNode}) {
     return <div className="bg-accent/5 min-h-screen">
-        {/* <NavBar /> */}
+        <NavBar />
         <div className="container py-6">
             {children}
         </div>
