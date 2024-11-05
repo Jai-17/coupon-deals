3924f5f8100faee78df847cfc822b8d51bbfd020
Jai-17 <madhukarjai9@gmail.com>
2024-11-06 01:44:31 +0530

    groundwork

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,15 @@
-export default function DashboardPage() {
-    return null;
+import { getProducts } from "@/server/db/products";
+import { auth } from "@clerk/nextjs/server";
+import { NoProducts } from "./_components/NoProduct";
+
+export default async function DashboardPage() {
+  const { userId, redirectToSignIn } = await auth();
+
+  if (userId == null) return redirectToSignIn();
+
+  const products = await getProducts(userId, { limit: 6 });
+
+  if (products.length === 0) return <NoProducts />;
+
+  return null;
 }
