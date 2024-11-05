a7ef673c6cd52f73e4f629fbac3b3c66ace9aec3
Jai-17 <madhukarjai9@gmail.com>
2024-11-05 20:22:33 +0530

    landing page added

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Button } from "@/components/ui/button"
-
-export default function HomePage() {
-  return <div className="flex justify-center h-screen items-center text-2xl">
-    <Button>Click Me</Button>
-  </div>
-}
