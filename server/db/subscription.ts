fe4b6cbcfd50a4a792170bfc93505f3236c922d5
Jai-17 <madhukarjai9@gmail.com>
2024-11-09 20:26:49 +0530

    fix4

--- server/db/subscription.ts
+++ server/db/subscription.ts
@@ -48,20 +48,26 @@ export async function getUserSubscriptionTier(userId: string) {
   return subscriptionTiers[subscription.tier];
 }
 
-export async function updateUserSubscription(where: SQL, data: Partial<typeof UserSubscriptionTable.$inferInsert>) {
-  const [updatedSubscription] = await db.update(UserSubscriptionTable).set(data).where(where).returning({
-    id: UserSubscriptionTable.id,
-    userId: UserSubscriptionTable.clerkUserId
-  })
+export async function updateUserSubscription(
+  where: SQL,
+  data: Partial<typeof UserSubscriptionTable.$inferInsert>
+) {
+  const [updatedSubscription] = await db
+    .update(UserSubscriptionTable)
+    .set(data)
+    .where(where)
+    .returning({
+      id: UserSubscriptionTable.id,
+      userId: UserSubscriptionTable.clerkUserId,
+    });
 
-  if(updatedSubscription != null) {
+  if (updatedSubscription != null) {
     revalidateDbCache({
       tag: CACHE_TAGS.subscription,
       userId: updatedSubscription.userId,
-      id: updatedSubscription.id
-    })
+      id: updatedSubscription.id,
+    });
   }
-
 }
 
 function getUserSubscriptionInternal(userId: string) {
